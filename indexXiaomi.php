90110da4fe92e09049f6d05830d0b9d59f0dc9e9
rauljprieto <76872302+rauljprieto@users.noreply.github.com>
2021-01-03 12:24:42 -0400

    Add files via upload

---
+++ indexXiaomi.php
@@ -0,0 +1,124 @@
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="robots" content="index,follow"/>
+        <meta name="author" content="RAUL PRIETO"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">        
+        <title>Xiaomi.com.ve - La tienda de Celulares Xiaomi mas completa</title>
+        <meta name='description' content='Te ofrecemos la mejor selección de productos  Xiaomi que se ofrecen en Amazon.'/>
+        <script type="text/javascript" src="http://www.centrodecomerciantesgonzalezcatan.org.ar/assets/vendors/jquery/jquery-3.3.1.min.js"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        <link href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet">
+        <style>
+        .header-logo{
+            margin-bottom: 0px;
+            padding: 10px;
+        }
+        .navbar-custom{
+            background-color: #00979d;
+        }
+        .navbar-custom > div > a{        
+            color: #fff;
+        }
+        .navbar-custom > div > div > ul > li > a{        
+            color: #fff;
+        }      
+        .navbar-btnMobile{
+            color:#fff;
+            font-family: 'Josefin Sans', sans-serif;  
+            margin-bottom: 0px;
+        }  
+        .navbar-active{
+            background-color: #fff;
+            color: #232f3e;
+            border-radius: 10px;            
+        }
+        .footer {
+            bottom: 0;
+            width: 100%;
+            height: 60px;
+            line-height: 60px; 
+            background-color: #444;
+            color:#fff;
+        }  
+        .footer > .container {
+            padding-right: 15px;
+            padding-left: 15px;
+        }     
+        .footer > .container > span{
+            color:#FFF;
+        }                         
+        .customFooterH5{
+            line-height:1em; 
+            font-family: 'Josefin Sans', sans-serif;  
+            color: #fff;          
+        }
+        .priceHeader
+        {
+            background-color: #00979d;
+            color: #fff;
+        }
+        .priceHeader > h2{
+            font-size: 19px;
+        }
+        .homeSeparador{
+            padding: 50px;
+            background-color: #febd69;
+            line-height: 1em;
+            color: #fff;
+            font-size: 50px;
+            font-family: 'Josefin Sans', sans-serif;
+            margin-bottom: 30px;            
+        }
+
+        </style>
+    </head>
+    <body>    
+        <!-- Header -->        
+        <header>
+            <div class="container">
+                <div class="col-md-12">
+                    <p class="text-center header-logo"><a class="nav-link" href="index.php"><img src="Images/Logo.png"></a></p>
+                </div>
+            </div>
+        </header>
+        <!-- Nav Bar -->
+        <nav class="navbar navbar-expand-lg navbar-custom">
+            <div class="container">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                   <p class="navbar-btnMobile">Menu</p>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ml-auto text-center">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="index.php">Inicio <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class='nav-item'><a class='nav-link' href='Moviles_Gama_Baja.php'>Moviles Gama Baja</a></li><li class='nav-item'><a class='nav-link' href='Moviles_Gama_Alta.php'>Moviles Gama Alta</a></li><li class='nav-item'><a class='nav-link' href='Moviles_Gama_Media.php'>Moviles Gama Media</a></li> 
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <!-- Contenido -->
+        <div class="container">
+            <div class="col-md-10 offset-md-1 text-center">
+                    <h1 class="mb-2 mt-5" style="line-height:1em; font-family: 'Josefin Sans', sans-serif;">Los mejores Telefonos Ziaomi del mercado</h1>   
+                    <p class="mb-5" style="font-family: 'Josefin Sans', sans-serif;"><b>¿Eres entusiasta de la tecnología o simplemente quieres estar comunicado en todo momento?</b>, Aquí podras encontrar la mayor gama de equipos Xiaomi, hicimos una selección de los mejores productos  en Amazon, y te lo traemos aquí para que no pierdas tiempo haciendo lo que ya hicimos nosotros. </p> 
+            </div>
+
+            <!-- Price Lis -->
+            <div class="container-fuild text-center">
+                <div class="row">
+                        <div class='col-md-12 text-center homeSeparador'> Moviles Gama Baja </div><div class='col-md-3'><div class='card mb-4 box-shadow'><div class='card-header priceHeader'><h2 class='my-0 font-weight-normal'>Xiaomi Redmi 9C NFC</h2></div><a href='https://www.amazon.es/gp/product/B08FMPXDTJ/ref=as_li_tl?ie=UTF8&creativeASIN=B08FMPXDTJ&tag=raulprieto-20' style='color: #333;text-decoration: none;'><div class='card-body'><img src='https://images-na.ssl-images-amazon.com/images/I/91hUmf%2BM-5L._AC_SL1500_.jpg' style='width: 100%;'><p>Smartphone con Pantalla HD+ de 6.53" DotDrop (3GB+64GB, Triple cámara trasera de 13MP con IA</p><button type='button' class='btn btn-lg btn-block btn-outline-primary' style='background-color: #febd69;color: #000;border: 0px;'>Ver en amazon</button></div></div></div></a><div class='col-md-3'><div class='card mb-4 box-shadow'><div class='card-header priceHeader'><h2 class='my-0 font-weight-normal'>Xiaomi Redmi Note 9S</h2></div><a href='https://www.amazon.es/gp/product/B085S4DSZH/ref=as_li_tl?ie=UTF8&creativeASIN=B085S4DSZH&tag=raulprieto-20' style='color: #333;text-decoration: none;'><div class='card-body'><img src='https://images-na.ssl-images-amazon.com/images/I/61t-zuLT0LL._AC_SL1200_.jpg' style='width: 100%;'><p>Smartphone de 6.67" FHD+ (DotDisplay, Snapdragon 720G, 4 GB RAM, 64 GB ROM, cámara cuádruple de 48 MP,</p><button type='button' class='btn btn-lg btn-block btn-outline-primary' style='background-color: #febd69;color: #000;border: 0px;'>Ver en amazon</button></div></div></div></a><div class='col-md-3'><div class='card mb-4 box-shadow'><div class='card-header priceHeader'><h2 class='my-0 font-weight-normal'>Xiaomi Mi Watch Lite</h2></div><a href='https://www.amazon.es/gp/product/B08P57P577/ref=as_li_tl?ie=UTF8&creativeASIN=B08P57P577&tag=raulprieto-20' style='color: #333;text-decoration: none;'><div class='card-body'><img src='https://images-na.ssl-images-amazon.com/images/I/71XOMpavALL._AC_SL1500_.jpg' style='width: 100%;'><p>Mi Watch Lite: Con un fino cuerpo cuadrado y un cristal curvado 2.5D que se funden de manera delicada y natural</p><button type='button' class='btn btn-lg btn-block btn-outline-primary' style='background-color: #febd69;color: #000;border: 0px;'>Ver en amazon</button></div></div></div></a><div class='col-md-3'><div class='card mb-4 box-shadow'><div class='card-header priceHeader'><h2 class='my-0 font-weight-normal'>Poco M3 Smartphone</h2></div><a href='https://www.amazon.es/gp/product/B08P4MVMDF/ref=as_li_tl?ie=UTF8&creativeASIN=B08P4MVMDF&tag=raulprieto-20' style='color: #333;text-decoration: none;'><div class='card-body'><img src='https://images-na.ssl-images-amazon.com/images/I/613iiDcD9UL._AC_SL1000_.jpg' style='width: 100%;'><p>Poco M3 Smartphone 4GB 128GB 6.53" FHD+ Large 1080P FHD+ Display 48MP Triple cámara 6000mAh batería Altavoces duales 3.5mm Headphone</p><button type='button' class='btn btn-lg btn-block btn-outline-primary' style='background-color: #febd69;color: #000;border: 0px;'>Ver en amazon</button></div></div></div></a><div class='col-md-3'><div class='card mb-4 box-shadow'><div class='card-header priceHeader'><h2 class='my-0 font-weight-normal'>Xiaomi Band 5</h2></div><a href='https://www.amazon.es/gp/product/B00SKF00RK/ref=as_li_tl?ie=UTF8&creativeASIN=B00SKF00RK&tag=raulprieto-20' style='color: #333;text-decoration: none;'><div class='card-body'><img src='https://images-na.ssl-images-amazon.com/images/I/51CoOESaU7L._AC_SL1000_.jpg' style='width: 100%;'><p>Xiaomi Band 5 - inteligente, 11 modos de deporte, pulsera de ejercicios, monitor de frecuencia cardíaca, monitor de sueño</p><button type='button' class='btn btn-lg btn-block btn-outline-primary' style='background-color: #febd69;color: #000;border: 0px;'>Ver en amazon</button></div></div></div></a><div class='col-md-3'><div class='card mb-4 box-shadow'><div class='card-header priceHeader'><h2 class='my-0 font-weight-normal'>Xiaomi Altavoz portátil Bluetooth 5.0</h2></div><a href='https://www.amazon.es/gp/product/B08LL3YYZB/ref=as_li_tl?ie=UTF8&creativeASIN=B08LL3YYZB&tag=raulprieto-20' style='color: #333;text-decoration: none;'><div class='card-body'><img src='https://images-na.ssl-images-amazon.com/images/I/61wzbk9Z6RL._AC_SL1000_.jpg' style='width: 100%;'><p>PX7 Barra Vertical estéreo inalámbrica a Prueba de Agua 13 Horas Tiempo de reproducción 16W Sonido Micrófono Incorporado, Conexión TWS </p><button type='button' class='btn btn-lg btn-block btn-outline-primary' style='background-color: #febd69;color: #000;border: 0px;'>Ver en amazon</button></div></div></div></a><div class='col-md-3'><div class='card mb-4 box-shadow'><div class='card-header priceHeader'><h2 class='my-0 font-weight-normal'>Xiaomi Mi Note 10 Lite</h2></div><a href='https://www.amazon.es/gp/product/B0881QJS9C/ref=as_li_tl?ie=UTF8&creativeASIN=B0881QJS9C&tag=raulprieto-20' style='color: #333;text-decoration: none;'><div class='card-body'><img src='https://images-na.ssl-images-amazon.com/images/I/81mtZIUNmhL._AC_SL1500_.jpg' style='width: 100%;'><p>(Pantalla FHD+ 6.47", 6GB + 64GB; Cámara 64MP, Snapdragon 730G, Dual 4G, 5260mAh con Carga rápida 30W, Android 10) </p><button type='button' class='btn btn-lg btn-block btn-outline-primary' style='background-color: #febd69;color: #000;border: 0px;'>Ver en amazon</button></div></div></div></a><div class='col-md-3'><div class='card mb-4 box-shadow'><div class='card-header priceHeader'><h2 class='my-0 font-weight-normal'>Xiaomi Mi 10T Pro</h2></div><a href='https://www.amazon.es/gp/product/B08HHCZ8YD/ref=as_li_tl?ie=UTF8&creativeASIN=B08HHCZ8YD&tag=raulprieto-20' style='color: #333;text-decoration: none;'><div class='card-body'><img src='https://images-na.ssl-images-amazon.com/images/I/81tgBV2lyRL._AC_SL1500_.jpg' style='width: 100%;'><p>Pantalla 6.67" Fhd+ Dotdisplay, 8Gb+256Gb, Cámara de 108Mp, Snapdragon 865 5G, 5.000Mah Comcarga 33W)</p><button type='button' class='btn btn-lg btn-block btn-outline-primary' style='background-color: #febd69;color: #000;border: 0px;'>Ver en amazon</button></div></div></div></a><div class='col-md-12 text-center homeSeparador'> Moviles Gama Media </div><div class='col-md-3'><div class='card mb-4 box-shadow'><div class='card-header priceHeader'><h2 class='my-0 font-weight-normal'>POCO X3 NFC</h2></div><a href='https://www.amazon.es/gp/product/B08B9C149J/ref=as_li_tl?ie=UTF8&creativeASIN=B08B9C149J&tag=raulprieto-20' style='color: #333;text-decoration: none;'><div class='card-body'><img src='https://images-na.ssl-images-amazon.com/images/I/71Plk3O3B8L._AC_SL1500_.jpg' style='width: 100%;'><p>Smartphone 6+128GB, 6,67” FHD+ cámara frontal con Punch-hole, Snapdragon 732G, 64 MP con IA, Quad-cámara, 5160 mAh, color Gris Sombra </p><button type='button' class='btn btn-lg btn-block btn-outline-primary' style='background-color: #febd69;color: #000;border: 0px;'>Ver en amazon</button></div></div></div></a><div class='col-md-3'><div class='card mb-4 box-shadow'><div class='card-header priceHeader'><h2 class='my-0 font-weight-normal'>Xiaomi Redmi Note 8 Pro </h2></div><a href='https://www.amazon.es/gp/product/B08FT88J8H/ref=as_li_tl?ie=UTF8&creativeASIN=B08FT88J8H&tag=raulprieto-20' style='color: #333;text-decoration: none;'><div class='card-body'><img src='https://images-na.ssl-images-amazon.com/images/I/61YbHSoI5wL._AC_SL1000_.jpg' style='width: 100%;'><p>Smartphone 64MP Quad Camera Mtk Heilo G90T Octa Core 6.53’’FHD NFC Mobile Phone Orange (6+128G)</p><button type='button' class='btn btn-lg btn-block btn-outline-primary' style='background-color: #febd69;color: #000;border: 0px;'>Ver en amazon</button></div></div></div></a><div class='col-md-3'><div class='card mb-4 box-shadow'><div class='card-header priceHeader'><h2 class='my-0 font-weight-normal'>Xiaomi Redmi Note 9 Pro</h2></div><a href='https://www.amazon.es/gp/product/B0888P87V2/ref=as_li_tl?ie=UTF8&creativeASIN=B0888P87V2&tag=raulprieto-20' style='color: #333;text-decoration: none;'><div class='card-body'><img src='https://images-na.ssl-images-amazon.com/images/I/81hw85-7thL._AC_SL1500_.jpg' style='width: 100%;'><p>Smartphone con pantalla FHD+ 6.67" DotDisplay (6 GB+128 GB, cámara cuádruple 64 MP con IA, SnapdragonTM 720G, batería 5020 mAh) Gris</p><button type='button' class='btn btn-lg btn-block btn-outline-primary' style='background-color: #febd69;color: #000;border: 0px;'>Ver en amazon</button></div></div></div></a><div class='col-md-3'><div class='card mb-4 box-shadow'><div class='card-header priceHeader'><h2 class='my-0 font-weight-normal'>Xiaomi Redmi Note 9S</h2></div><a href='https://www.amazon.es/gp/product/B085S4DSZH/ref=as_li_tl?ie=UTF8&creativeASIN=B085S4DSZH&tag=raulprieto-20' style='color: #333;text-decoration: none;'><div class='card-body'><img src='https://images-na.ssl-images-amazon.com/images/I/61t-zuLT0LL._AC_SL1200_.jpg' style='width: 100%;'><p>Smartphone de 6.67" FHD+ (DotDisplay, Snapdragon 720G, 4 GB RAM, 64 GB ROM, cámara cuádruple de 48 MP, bateria de 5020mAh</p><button type='button' class='btn btn-lg btn-block btn-outline-primary' style='background-color: #febd69;color: #000;border: 0px;'>Ver en amazon</button></div></div></div></a><div class='col-md-3'><div class='card mb-4 box-shadow'><div class='card-header priceHeader'><h2 class='my-0 font-weight-normal'>Xiaomi Redmi Note 9</h2></div><a href='https://www.amazon.es/gp/product/B0881TT4HL/ref=as_li_tl?ie=UTF8&creativeASIN=B0881TT4HL&tag=raulprieto-20' style='color: #333;text-decoration: none;'><div class='card-body'><img src='https://images-na.ssl-images-amazon.com/images/I/61EG9Kb2pPL._AC_SL1000_.jpg' style='width: 100%;'><p>Xiaomi Redmi Note 9 - Smartphone de 6.53” FHD (DotDisplay, 4 GB RAM, 128 GB ROM, cámara Quad de 48 MP, Hotshot 3.5 mm, Headphone Jack, batería de 5020 mAh</p><button type='button' class='btn btn-lg btn-block btn-outline-primary' style='background-color: #febd69;color: #000;border: 0px;'>Ver en amazon</button></div></div></div></a><div class='col-md-3'><div class='card mb-4 box-shadow'><div class='card-header priceHeader'><h2 class='my-0 font-weight-normal'>Xiaomi Redmi 9</h2></div><a href='https://www.amazon.es/gp/product/B089WCC775/ref=as_li_tl?ie=UTF8&creativeASIN=B089WCC775&tag=raulprieto-20' style='color: #333;text-decoration: none;'><div class='card-body'><img src='https://images-na.ssl-images-amazon.com/images/I/51cGn9i%2Ba%2BL._AC_SL1000_.jpg' style='width: 100%;'><p>Smartphone 3GB 32GB, Pantalla FHD+ de 6.53", AI Quad Cámara de 13 MP, Batería de 5020 mAh</p><button type='button' class='btn btn-lg btn-block btn-outline-primary' style='background-color: #febd69;color: #000;border: 0px;'>Ver en amazon</button></div></div></div></a><div class='col-md-3'><div class='card mb-4 box-shadow'><div class='card-header priceHeader'><h2 class='my-0 font-weight-normal'>Xiaomi Redmi 9A</h2></div><a href='https://www.amazon.es/gp/product/B08CH6X6M5/ref=as_li_tl?ie=UTF8&creativeASIN=B08CH6X6M5&tag=raulprieto-20' style='color: #333;text-decoration: none;'><div class='card-body'><img src='https://images-na.ssl-images-amazon.com/images/I/51bseWUDNOL._AC_SL1000_.jpg' style='width: 100%;'><p>Xiaomi Redmi 9A - Smartphone 32GB, 2GB RAM, Dual Sim, Sky Blue</p><button type='button' class='btn btn-lg btn-block btn-outline-primary' style='background-color: #febd69;color: #000;border: 0px;'>Ver en amazon</button></div></div></div></a>
+                </div>           
+            </div>            
+        </div>
+        <footer class="footer backFooter">
+                <div class="container">
+                    <span>Copyright © 2021 Participamos en el sistema de afiliados de Amazon España.  <a href="http://raulprieto.com.ve" style="color: #fff;">RaulPrieto</a> • <a href="avisolegal.php" style="color: #fff;">Aviso legal y Politicas de Privacidad</a></span>
+                </div>
+        </footer>    
+    </body>
+</html>
